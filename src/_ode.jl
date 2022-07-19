298c3441eb8202bb95fb8c2a6df02e7c09ea91de
Hiroyuki Okamura <okamu@hiroshima-u.ac.jp>
2022-07-19 13:02:20 +0000

    add

--- src/_ode.jl
+++ src/_ode.jl
@@ -83,11 +83,12 @@ function simulate(prob::MBDProblem, tspan; n = 1000, alg=DifferentialEquations.T
 end
 
 function simulate(blk::AbstractBlock, pdict, tspan; n = 1000, alg=DifferentialEquations.Tsit5(), kwargs...)
-    params = (;pdict...)
+    params = get_parameters(blk)
     iv = blk.ifunc(params)
     p = DifferentialEquations.ODEProblem(blk.sfunc, iv, tspan, params)
     sol = DifferentialEquations.solve(p, alg; kwargs...)
     ts = LinRange(tspan[1], tspan[2], n)
     results = blk.ofunc(sol, params, ts)
-    # Plots.plot(ts, [x for (_,x) = results], layout=(length(results),1), leg=false)
+    graph = Plots.plot(ts, [x for (_,x) = results], layout=(length(results),1), leg=false)
+    (params=params, solution=sol, ts=ts, outputs=results, graph=graph)
 end
