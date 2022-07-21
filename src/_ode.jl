74be5be73eee9ca305cbc409b64b433611dd48b7
Hiroyuki Okamura <okamu@hiroshima-u.ac.jp>
2022-07-21 04:38:49 +0000

    add

--- src/_ode.jl
+++ src/_ode.jl
@@ -6,7 +6,8 @@ function simulate(blk::AbstractSystemBlock, tspan; n = 1000, alg=DifferentialEqu
     sol = DifferentialEquations.solve(p, alg; kwargs...)
     ts = LinRange(tspan[1], tspan[2], n)
     results = blk.ofunc(sol, params, ts)
-    graph = Plots.plot(ts, [x for (_,x) = results], layout=(length(results),1), leg=false)
+    graph = Plots.plot(ts, [x for (_,x) = results], layout=(length(results),1),
+        title=reshape([x for (x,_) = results], 1, length(results)), leg=false)
     (params=params, solution=sol, ts=ts, outputs=results, graph=graph)
 end
 
@@ -14,6 +15,7 @@ function simulate(blk::AbstractFunctionBlock, tspan; n = 1000, alg=DifferentialE
     params = (;get_parameters(blk)...)
     ts = LinRange(tspan[1], tspan[2], n)
     results = blk.ofunc(0, params, ts)
-    graph = Plots.plot(ts, [x for (_,x) = results], layout=(length(results),1), leg=false)
+    graph = Plots.plot(ts, [x for (_,x) = results], layout=(length(results),1),
+        title=reshape([x for (x,_) = results], 1, length(results)), leg=false)
     (params=params, ts=ts, outputs=results, graph=graph)
 end
