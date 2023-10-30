d154d6fb8ebddd891110f86c570a7c9088b534be
Hiroyuki Okamura <okamu@hiroshima-u.ac.jp>
2023-10-30 08:25:04 +0000

    mbd

--- src/predefined/Discontinuities/Quantizer.jl
+++ src/predefined/Discontinuities/Quantizer.jl
@@ -3,17 +3,17 @@ export Quantizer
 function Quantizer(;
     in = InPort(),
     out = OutPort(),
-    quantizationinterval = Float64(1))
+    qinterval = Float64(1))
     b = SimpleBlock(:Quantizer)
     setport!(b, :in, in)
     setport!(b, :out, out)
-    setparameter!(b, :quantizationinterval, quantizationinterval)
+    setparameter!(b, :qinterval, qinterval)
     b
 end
 
 function expr(b::SimpleBlock, ::Val{:Quantizer})
     in = b.inports[1].name
-    quantizationinterval = b.env[:quantizationinterval].name
+    qinterval = b.env[:qinterval].name
     out = b.outports[1].name
-    :($out = $quantizationinterval * round($in / $quantizationinterval))
+    :($out = $qinterval * round($in / $qinterval))
 end
